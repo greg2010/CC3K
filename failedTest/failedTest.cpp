da21816cbe045c629e7ca77ae3d3d1d44879c630
greg2010 <greg2008200@gmail.com>
2016-07-11 00:27:41 +0000

    Terrible issue fix

--- failedTest/failedTest.cpp
+++ failedTest/failedTest.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-  std::cerr << "This will exit with non-zero exit code!" << std::endl;
-  return 1;
+  // std::cerr << "This will exit with non-zero exit code!" << std::endl;
+  std::cout << "Not anymore!" << std::endl;
+  //return 1;
+  return 0;
 }
