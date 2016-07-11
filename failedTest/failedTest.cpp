4cbb1de8342f28fdd2b94a6e4f66b702ca46b2b2
greg2010 <greg2008200@gmail.com>
2016-07-11 17:35:03 -0400

    remove stuff

--- failedTest/failedTest.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-  // std::cerr << "This will exit with non-zero exit code!" << std::endl;
-  std::cout << "Not anymore!" << std::endl;
-  //return 1;
-  return 0;
-}
