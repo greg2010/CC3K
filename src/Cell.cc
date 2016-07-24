73b1497ac8acc30b70d5893678a37b91e08b0189
greg2010 <greg2008200@gmail.com>
2016-07-24 18:51:40 -0400

    Copy/move ctors for Cell

--- src/Cell.cc
+++ src/Cell.cc
@@ -5,3 +5,17 @@ Cell::Cell(std::pair<int, int> coords) : Subject{coords}, element{nullptr} { }
 std::shared_ptr<Subject> &Cell::operator->() const {
     return element;
 }
+
+Cell &Cell::operator=(std::shared_ptr<Subject> &rhs) {
+    element = rhs;
+    return *this;
+}
+
+Cell &Cell::operator=(std::shared_ptr<Subject> &&rhs) {
+    element = rhs;
+    return *this;
+}
+
+Cell::Cell(Cell &rhs) : Subject{rhs.getCoordinates()} { }
+
+Cell::Cell(Cell &&rhs) : Subject{rhs.getCoordinates()} { }
--- src/Cell.h
+++ src/Cell.h
@@ -7,6 +7,10 @@
 class Cell : public Subject {
 public:
     Cell(std::pair<int,int> coords);
+    Cell(Cell &rhs);
+    Cell(Cell &&rhs);
+    Cell &operator=(std::shared_ptr<Subject> &rhs);
+    Cell &operator=(std::shared_ptr<Subject> &&rhs);
     std::shared_ptr<Subject> &operator->() const;
 private:
     std::shared_ptr<Subject> element;
